19fa34d904aadea747883e0df4c19acbce7648d7
IRdotAI <abrasive.tax@gmail.com>
2026-08-18 23:58:51 +0100

    Deploy 202608182358

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE on every deploy so phones pick up new sprite data.
-const CACHE = 'sprite-index-v202608182335';
+const CACHE = 'sprite-index-v202608182358';
 const ASSETS = ['./', './index.html', './data/sprites.json', './manifest.json'];
 
 self.addEventListener('install', e => {
