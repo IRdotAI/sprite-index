9e0d3b334247500ce4b9e0ce958f80393f916a85
IRdotAI <abrasive.tax@gmail.com>
2026-08-19 00:23:46 +0100

    Sync collection 202608190023

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE on every deploy so phones pick up new sprite data.
-const CACHE = 'sprite-index-v202608182358';
+const CACHE = 'sprite-index-v202608190023';
 const ASSETS = ['./', './index.html', './data/sprites.json', './manifest.json'];
 
 self.addEventListener('install', e => {
